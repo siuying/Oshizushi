b4095199b2e8a82ce9f405995bc659bb61639c2f
Francis Chong <francis@ignition.hk>
2014-02-21 10:44:31 +0800

    Initial Commit

---
+++ Oshizushi/IGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IGAppDelegate.h
+//  Oshizushi
+//
+//  Created by Francis Chong on 2/21/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
