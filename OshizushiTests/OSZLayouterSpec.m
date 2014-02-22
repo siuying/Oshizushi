6d51427091d21ae1f506e65380ed73c15e2e0e12
Francis Chong <francis@ignition.hk>
2014-02-22 22:45:13 +0800

    add test

--- OshizushiTests/OSZLayouterSpec.m
+++ OshizushiTests/OSZLayouterSpec.m
@@ -48,6 +48,20 @@ describe(@"OSZLayouter", ^{
             [[theValue(view.autoresizingMask & UIViewAutoresizingFlexibleBottomMargin) should] equal:theValue(UIViewAutoresizingFlexibleBottomMargin)];
         });
     });
+    
+    context(@"Pin to both edge", ^{
+        it(@"should layout |-5-[view]-5-|", ^{
+            UIView* view = [[UIView alloc] initWithFrame:CGRectZero];
+            [superview addSubview:view];
+            
+            NSDictionary* views = NSDictionaryOfVariableBindings(view);
+            [layouter layoutWithVisualFormat:@"H:|-5-[view]-5-|" metrics:nil views:views];
+            
+            [[theValue(view.frame.origin.x) should] equal:theValue(5)];
+            [[theValue(view.frame.origin.y) should] equal:theValue(0)];
+            [[theValue(view.frame.size.width) should] equal:theValue(310)];
+        });
+    });
 });
 
 SPEC_END
